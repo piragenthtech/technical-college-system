752652e44db8c1abd3e4b18cef56e73cf05cf8e3
piragenth-ltfb <111213108+piragenth-ltfb@users.noreply.github.com>
2024-07-23 17:00:18 +0530

    fd

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <nav className="headernav">
+      <a className="headerlink" href="students">
+        Students
+      </a>
+      <h3 className="headerheading">Technical College System</h3>
+      <a className="headerlink" href="computers">
+        Computers
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
