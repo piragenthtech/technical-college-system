b2d4bdc8928d365c7a0253442cc2dfb8120fa205
piragenth-ltfb <111213108+piragenth-ltfb@users.noreply.github.com>
2024-07-25 22:26:48 +0530

    data

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-const express =requ
+const express = require("express");
+const app = express();
+var cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1>This is from node js server</h1>");
+});
+
+app.listen(8080);
