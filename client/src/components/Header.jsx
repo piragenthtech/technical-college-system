af88cb060d150a8b43c135a1a8a110172d4ce0ef
piragenthtech <piragenthtech@gmail.com>
2024-08-12 10:49:34 +0530

    header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,29 +3,24 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 export default function Header() {
   const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
   return (
-    <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-      <Nav.Item>
-        <Nav.Link eventKey="1" href="#/home">
-          NavLink 1 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" title="Item">
-          NavLink 2 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3" disabled>
-          NavLink 3 content
-        </Nav.Link>
-      </Nav.Item>
-      <NavDropdown title="Dropdown" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
+    <div className="headerhead">
+      <a href="/" className="headerlogo">
+        Technical College system
+      </a>
+      <nav>
+      <a href="Studentsget" className="link">
+          StudentsGet
+        </a>
+        <a href="Students" className="link">
+          Students
+        </a>
+        <a href="Staffs" className="link">
+          Staffs
+        </a>
+        <a href="Computers" className="link">
+          Computers
+        </a>
+      </nav>
+    </div>
   );
 }
